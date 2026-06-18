e17f52e107d3a83a887eed7eb9268cf732fc7dd2
Kris Lee <kdlee0401@gmail.com>
2026-06-18 18:25:21 -0500

    Refactored and simplified some code.

--- character.js
+++ character.js
@@ -4,34 +4,33 @@
 alert("Welcome traveller... to Aerova, the floating sky city! I am Kris, your guide and I will help you create your hero.");
 const charName = prompt("What is your name, traveller?", "Traveller") || "Wanderer";
 // Check for pets
-let petNameTemp = "";
+let petNameTemp = "None";
 let hasPet = confirm(`${charName}, do you have a companion?\n\nOK for YES\nCANCEL for NO.`);
 
 if (hasPet) {
     petNameTemp = prompt(`What kind of companion do you have ${charName}? (e.g. Cat, Wolf, Dragon, etc.)`, "companion") || "Rock";
 } else {
-    alert("That's a lonely life.");
-    petNameTemp = "None";
+    alert("That's boring!");
 }
 
 const petName = petNameTemp;
 
 // Check for any super powers
-let power = "";
+let powerTemp = "None";
 let trainingTemp = 0;
 let hasPowers = confirm(`Do you have any super powers, ${charName}? Like magic, super strength, or extreme luck?\n\nOK for YES\nCANCEL for NO.`);
 
 if (hasPowers) {
-    power = prompt("What is your power?", "being sleep deprived") || "None";
-    trainingTemp = prompt(`How many years have you been training your skill, ${power}?`, 1) || 1;
+    powerTemp = prompt("What is your power?", "being sleep deprived") || "None";
+    trainingTemp = prompt(`How many years have you been training your skill, ${powerTemp}?`, 1) || 1;
 } else {
     alert("oh, so you're useless. Okay...");
-    power = "None";
 }
 
-const superPower = power;
+const superPower = powerTemp;
 const trainingYearsText = trainingTemp;
-const trainingYears = parseInt(trainingYearsText);
+const trainingYears = isNaN(parseInt(trainingYearsText)) ? 0 : parseInt(trainingYearsText);
+const yearLabel = trainingYears === 1 ? "year" : "years";
 console.log(trainingYearsText, typeof trainingYearsText);
 console.log(trainingYears, typeof trainingYears);
 const powerLevel = trainingYears * 10;
@@ -41,11 +40,11 @@ console.log(likesFighting, typeof likesFighting);
 
 let dialogueChoice = "";
 
-if (superPower != "None") {
-    dialogueChoice = `With ${trainingYears} ${trainingYears > 1 || trainingYears == 0 ? 'years' : 'year'} of training ${superPower}${petName != "None" ? ` and along with your 
-    ${petName} companion,` : ` ,`} there is no doubt you will save Aerova!`;
+if (superPower !== "None") {
+    dialogueChoice = `With ${trainingYears} ${(yearLabel)} of training ${superPower}${petName !== "None" ? ` and along with your 
+    ${petName} companion,` : `,`} there is no doubt you will save Aerova!`;
 } else {
-    dialogueChoice = `However, without any abilities ${petName != "None" ? `and even with your ${petName}` : "or a companion"}, you can't save Aerova!`;
+    dialogueChoice = `However, without any abilities ${petName !== "None" ? `and even with your ${petName}` : "or a companion"}, you can't save Aerova!`;
 }
 
 alert(`Gathering magic for ${charName}... almost ready!`);
